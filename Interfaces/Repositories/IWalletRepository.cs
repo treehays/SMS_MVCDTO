5fec35bdc6928ea585871207d5c81ac4044bc633
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-12-17 17:45:55 +0100

    Some foldrer structure

---
+++ Interfaces/Repositories/IWalletRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace SMS_MVCDTO.Interfaces.Repositories
+{
+    public interface IWalletRepository
+    {
+    }
+}
---
+++ Interfaces/Services/IUserService.cs
@@ -0,0 +1,6 @@
+﻿namespace SMS_MVCDTO.Interfaces.Services
+{
+    public interface IUserService
+    {
+    }
+}
