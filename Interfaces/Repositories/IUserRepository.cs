74c737dc991cbb8974ff2aa92cc32f5764529ac0
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-12-17 20:27:03 +0100

    User Repo done

--- Interfaces/Repositories/IUserRepository.cs
+++ Interfaces/Repositories/IUserRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace SMS_MVCDTO.Interfaces.Repositories
+﻿using SMS_MVCDTO.Models.Entities;
+
+namespace SMS_MVCDTO.Interfaces.Repositories
 {
     public interface IUserRepository
     {
+        User Create(User user);
+        User Login(User user);
+        void Delete(User user);
+        User GetById(string staffId);
+        User GetByEmail(string email);
+        User GetPhoneNumber(string phoneNumber);
+        IList<User> GetByName(string email);
+        IList<User> GetAll();
+        IList<User> GetAttendants();
+        IList<User> GetSalesManagers();
+        User Update(User user);
+        User UpdatePassword(User user);
     }
 }
