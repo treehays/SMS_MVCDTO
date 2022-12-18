8dfbf2057d974bae895ab6bcf7aeab8a0e0eb452
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-12-18 15:44:50 +0100

    restructure entities

--- Models/Entities/Customer.cs
+++ Models/Entities/Customer.cs
@@ -2,22 +2,36 @@
 
 namespace SMS_MVCDTO.Models.Entities
 {
-    public class Customer : BaseEntity 
+    public class Customer //: BaseEntity 
     {
-        public IList<Transaction> Transactions { get; set; }
-        public Wallet Wallets { get; set; }
+        //public IList<Transaction> Transactions { get; set; }
+        //public Wallet Wallets { get; set; }
         public User User { get; set; }
         public string UserId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public string Email { get; set; }
         //public string Pin { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public bool Status { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public GenderType Gender { get; set; }
-        public MaritalStatusType MaritalStatus { get; set; }
-        public UserRoleType userRole { get; set; }
+        //public string PhoneNumber { get; set; }
+        //public string Address { get; set; }
+        //public bool Status { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+        //public GenderType Gender { get; set; }
+        //public MaritalStatusType MaritalStatus { get; set; }
+        //public UserRoleType userRole { get; set; }
+    }
+
+    public class Staff //: BaseEntity
+    {
+        //public IList<Transaction> Transactions { get; set; }
+        //public Wallet Wallets { get; set; }
+        public User User { get; set; }
+        public string UserId { get; set; }
+        //Attendant 
+        public string ResidentialAddress { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string BankName { get; set; }
+        public string GuarantorName { get; set; }
+        public string GuarantorPhoneNumber { get; set; }
     }
 }
