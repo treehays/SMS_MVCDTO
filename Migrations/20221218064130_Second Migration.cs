bd2ef5276c81fa558c30a8f70adb2e11e5182221
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-12-18 07:44:31 +0100

    Database Migrated

---
+++ Migrations/20221218064130_Second Migration.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMSMVCDTO.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductTransaction_Product_ProductId",
+                table: "ProductTransaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductTransaction_Transaction_TransactionId",
+                table: "ProductTransaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Users_CustomerId",
+                table: "Transaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallet_Transaction_TransactionId",
+                table: "Wallet");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallet_Users_CustomerId",
+                table: "Wallet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Wallet",
+                table: "Wallet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductCategory",
+                table: "ProductCategory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "Wallet",
+                newName: "Wallets");
+
+            migrationBuilder.RenameTable(
+                name: "Transaction",
+                newName: "Transactions");
+
+            migrationBuilder.RenameTable(
+                name: "ProductCategory",
+                newName: "ProductCategories");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Users",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Users",
+                newName: "ResidentialAddress");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallet_TransactionId",
+                table: "Wallets",
+                newName: "IX_Wallets_TransactionId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallet_CustomerId",
+                table: "Wallets",
+                newName: "IX_Wallets_CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transaction_CustomerId",
+                table: "Transactions",
+                newName: "IX_Transactions_CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_ProductCategoryId",
+                table: "Products",
+                newName: "IX_Products_ProductCategoryId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankAccountNumber",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankName",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GuarantorName",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GuarantorPhoneNumber",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HomeAddress",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Transactions",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Wallets",
+                table: "Wallets",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductCategories",
+                table: "ProductCategories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FirstName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pin = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PhoneNumber = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Address = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    MaritalStatus = table.Column<int>(type: "int", nullable: false),
+                    userRole = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Modified = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductCategories_ProductCategoryId",
+                table: "Products",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductTransaction_Products_ProductId",
+                table: "ProductTransaction",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductTransaction_Transactions_TransactionId",
+                table: "ProductTransaction",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Customers_CustomerId",
+                table: "Transactions",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Customers_CustomerId",
+                table: "Wallets",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Transactions_TransactionId",
+                table: "Wallets",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductCategories_ProductCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductTransaction_Products_ProductId",
+                table: "ProductTransaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductTransaction_Transactions_TransactionId",
+                table: "ProductTransaction");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Customers_CustomerId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Customers_CustomerId",
+                table: "Wallets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Transactions_TransactionId",
+                table: "Wallets");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Wallets",
+                table: "Wallets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductCategories",
+                table: "ProductCategories");
+
+            migrationBuilder.DropColumn(
+                name: "BankAccountNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BankName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GuarantorName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "GuarantorPhoneNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HomeAddress",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Wallets",
+                newName: "Wallet");
+
+            migrationBuilder.RenameTable(
+                name: "Transactions",
+                newName: "Transaction");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Product");
+
+            migrationBuilder.RenameTable(
+                name: "ProductCategories",
+                newName: "ProductCategory");
+
+            migrationBuilder.RenameColumn(
+                name: "ResidentialAddress",
+                table: "Users",
+                newName: "Address");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Users",
+                newName: "Status");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallets_TransactionId",
+                table: "Wallet",
+                newName: "IX_Wallet_TransactionId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallets_CustomerId",
+                table: "Wallet",
+                newName: "IX_Wallet_CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_CustomerId",
+                table: "Transaction",
+                newName: "IX_Transaction_CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_ProductCategoryId",
+                table: "Product",
+                newName: "IX_Product_ProductCategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Transaction",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Wallet",
+                table: "Wallet",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductCategory",
+                table: "ProductCategory",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductCategory_ProductCategoryId",
+                table: "Product",
+                column: "ProductCategoryId",
+                principalTable: "ProductCategory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductTransaction_Product_ProductId",
+                table: "ProductTransaction",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductTransaction_Transaction_TransactionId",
+                table: "ProductTransaction",
+                column: "TransactionId",
+                principalTable: "Transaction",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Users_CustomerId",
+                table: "Transaction",
+                column: "CustomerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallet_Transaction_TransactionId",
+                table: "Wallet",
+                column: "TransactionId",
+                principalTable: "Transaction",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallet_Users_CustomerId",
+                table: "Wallet",
+                column: "CustomerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
