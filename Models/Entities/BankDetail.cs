8e67785f09a106dfe71001561ddd95d988af84f3
ABDULSALAM Ahmad Ayoola <33053528+treehays@users.noreply.github.com>
2023-02-27 21:00:49 +0100

    Update BankDetail.cs

--- Models/Entities/BankDetail.cs
+++ Models/Entities/BankDetail.cs
@@ -2,12 +2,14 @@
 {
     public class BankDetail : BaseEntity
     {
-        public Attendant Attendant { get; set; }
-        public int AttendantId { get; set; }
-        public SalesManager SalesManager { get; set; }
-        public int SalesManagerId { get; set; }
-        public SuperAdmin SuperAdmin { get; set; }
-        public int SuperAdminId { get; set; }
+       // public Attendant Attendant { get; set; }
+      //  public int AttendantId { get; set; }
+       // public SalesManager SalesManager { get; set; }
+        //public int SalesManagerId { get; set; }
+        //public SuperAdmin SuperAdmin { get; set; }
+       // public int SuperAdminId { get; set; }
+public User User { get; set; }
+public int UserId { get; set; }
         public string BankAccountNumber { get; set; }
         public string BankName { get; set; }
         public string AccountType { get; set; }
