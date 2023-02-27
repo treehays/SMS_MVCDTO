913fc1c52c86aa29007d8113299ea9953f9a8a3c
ABDULSALAM Ahmad Ayoola <33053528+treehays@users.noreply.github.com>
2023-02-27 21:03:01 +0100

    Update SuperAdmin.cs

--- Models/Entities/SuperAdmin.cs
+++ Models/Entities/SuperAdmin.cs
@@ -10,8 +10,8 @@ namespace SMS_MVCDTO.Models.Entities
     {
         public User User { get; set; }
         public int UserId { get; set; }
-        public BankDetail BankDetail { get; set; }
-        public Address Address { get; set; }
+        //public BankDetail BankDetail { get; set; }
+     //   public Address Address { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
